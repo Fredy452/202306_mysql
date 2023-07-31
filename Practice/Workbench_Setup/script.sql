c92898aaa33d69636fb8f37624018aa67e22aa9b
Fred452 <fredyarce452@gmail.com>
2023-07-30 22:22:07 -0400

    more changes

--- Practice/Workbench_Setup/script.sql
+++ Practice/Workbench_Setup/script.sql
@@ -1,34 +1,30 @@
--- 1. Consulta: crea 3 usuarios nuevos
-INSERT INTO users(nombre, apellido, correo_electronico, created_at, updated_at) 
-VALUES('Marcos', 'Alonso', 'correo@correo.com', now(), now());
-INSERT INTO users(nombre, apellido, correo_electronico, created_at, updated_at) 
-VALUES('Lucas', 'Fernandez', 'correo2@correo.com', now(), now());
-INSERT INTO users(nombre, apellido, correo_electronico, created_at, updated_at) 
-VALUES('Jose', 'Duran', 'correo3@correo.com', now(), now());
-
--- 2. Consulta: recupera todos los usuarios
-SELECT * FROM users;
-
--- 3. Consulta. recupera el primer usuario que usa su dirección de correo electrónico
-SELECT * 
-FROM users
-WHERE correo_electronico IS NOT NULL;
-
--- 4. Consulta: recupera el último usuario que usa su id
-SELECT * FROM users WHERE users.id = 1;
-
--- 5. Consulta: cambia el usuario con id=3 para que su apellido sea Panqueques
-UPDATE users SET nombre = 'Panqueque'
-WHERE users.id = 3;
-
--- 6. Consulta: elimina el usuario con id=2 de la base de datos
-DELETE FROM users 
-where id = 2;
-
--- 7. Consulta: obtén todos los usuarios, ordenados por su nombre
-SELECT * FROM users
-ORDER BY nombre;
-
--- 8. Consulta BONUS: obtén todos los usuarios, ordenados por su nombre en orden descendente
-SELECT * FROM users
-ORDER BY nombre DESC;
+-- 1. Consulta: crea 3 dojos nuevos
+INSERT INTO dojos(nombre) VALUES('Programacion');
+INSERT INTO dojos(nombre) VALUES('Diseño');
+INSERT INTO dojos(nombre) VALUES('Prueba');
+-- 2. Consulta: elimina los 3 dojos que acabas de crear
+DELETE FROM dojos WHERE dojos.id in (1,2,3);
+-- 3. Consulta: crea 3 dojos nuevos
+INSERT INTO dojos(nombre) VALUES('Programacion'), ('Diseño'), ('Prueba');
+-- 4. Consulta: crea 3 ninjas que pertenezcan al primer dojo
+INSERT INTO ninjas(nombre, apellido, edad, dojo_id) VALUES('Jose', 'Cardozo', 12, 4),
+														  ('Ariel', 'Ben', 12, 4),
+                                                          ('Arturo', 'Cardozo', 12, 4);
+-- 5. Consulta: crea 3 ninjas que pertenezcan al segundo dojo
+INSERT INTO ninjas(nombre, apellido, edad, dojo_id) VALUES('Luis', 'Cardozo', 12, 5),
+														  ('Marcos', 'Cardozo', 12, 5),
+                                                          ('Pedro', 'Cardozo', 12, 5);
+-- 6. Consulta: crea 3 ninjas que pertenezcan al tercer dojo
+INSERT INTO ninjas(nombre, apellido, edad, dojo_id) VALUES('Diego', 'Perez', 12, 6),
+														  ('Mario', 'Ar', 12, 6),
+                                                          ('En', 'Joz', 12, 6);
+                                                          
+-- 7.Consulta: recupera todos los ninjas del primer dojo
+SELECT * FROM ninjas WHERE dojo_id = 4;
+-- 8. Consulta: recupera todos los ninjas del último dojo
+SELECT * FROM ninjas WHERE dojo_id = 6;
+-- 9. Consulta: recupera el dojo del último ninja
+SELECT dojos.nombre
+FROM dojos
+JOIN ninjas ON ninjas.dojo_id = dojos.id
+WHERE ninjas.id = 11;
